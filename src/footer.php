d6ca9dd22d0c2c882c67e5f1db60861c94ca16f2
Sean Chen <seanchen4@gmail.com>
2012-05-17 16:44:01 -0700

    restructure git

--- src/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="left-align">
-Whingit © 2012 · English (US)
-</div>
-
-<div class="right-align">
-About · Advertising · Create a Page · Developers · Careers · Privacy · Terms · Help
-</div>
