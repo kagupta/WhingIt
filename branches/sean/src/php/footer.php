1add3f03d3582eb7410292889c54912971a91ef6
Sean Chen <seanchen4@gmail.com>
2012-05-17 17:17:40 -0700

    incorporate code into header and footer

--- branches/sean/src/php/footer.php
+++ branches/sean/src/php/footer.php
@@ -1,7 +1,18 @@
-<div class="left-align">
-Whingit © 2012 · English (US)
-</div>
+<div class="footer">
+  <div class="left-align">
+  Whingit © 2012 · English (US)
+  </div>
+  
+  <div class="right-align">
+  About · Advertising · Create a Page · Developers · Careers · Privacy · Terms · Help
+  </div>
 
-<div class="right-align">
-About · Advertising · Create a Page · Developers · Careers · Privacy · Terms · Help
-</div>
+  </div><!-- end .footer -->
+  
+  <div class="clearfloat"></div>
+</div><!-- end .container -->
+
+</div><!-- end .container1 -->
+
+</body>
+</html>
