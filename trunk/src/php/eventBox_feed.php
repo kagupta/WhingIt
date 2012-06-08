f3a230e05738dc6776e13b8db3df5510889c61c6
Kapil Gupta <kap.kgp@gmail.com>
2012-06-08 14:19:45 -0700

    Displaying default image in case no image is provided

--- trunk/src/php/eventBox_feed.php
+++ trunk/src/php/eventBox_feed.php
@@ -1,5 +1,11 @@
 <?php
   $row = $_GET['event_info'];
+  if(isset($row['id'])) {
+    $id = $row['id'];
+  } else {
+    $id = 0;
+  }
+
   $userID = $_SESSION['id'];
   $attending = 1;
   $notAttending = 2;
@@ -35,7 +41,7 @@ function attending(eventID, status)
 </script>
 
 <div class="eventbox" id="<?php echo $row['id']; ?>">
-<img src="/res/images/photo.jpg" width="70" height="70" style="margin: 5px 10px 10px 0px; float:left;vertical-align: bottom;">
+<img  src="/src/php/image.php?eid=<?php echo $id;?>" width="70" height="70" style="margin: 5px 10px 10px 0px; float:left;vertical-align: bottom;">
 <?php
   echo "<h1 class=\"eventbox_text\">" . $row['name'] . "</h1>";
   echo "<h2 class=\"eventbox_text\">" . $row['location'] . "</h2>"; 
