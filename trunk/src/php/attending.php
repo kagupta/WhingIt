b26e25cf6ef7005ce44ce180dd2c9dcd2249d84a
ansumd@gmail.com <Ansum@Ansum-PC.(none)>
2012-06-08 18:02:56 -0700

    Fixed bugs in attending feature

--- trunk/src/php/attending.php
+++ trunk/src/php/attending.php
@@ -15,13 +15,25 @@ mysql_select_db("whingit", $link);
 	$userID = $_SESSION['id'];
 	$eventID = $_GET['eventID'];
 	$attStatus = $_GET['status'];
-	mysql_query("INSERT INTO attend (id, attendee, status) VALUES ('$eventID','$userID','$attStatus')");
+	$currentStatus = mysql_query("SELECT status FROM attend WHERE attend.id=$eventID AND attend.attendee=$userID");
+	$totalRows = mysql_num_rows($currentStatus);
+	switch($totalRows)
+	{
+		case 0:
+			mysql_query("INSERT INTO attend (id, attendee, status) VALUES ('$eventID','$userID','$attStatus')");
+			break;
+		default:
+			$temp = mysql_result($currentStatus, 0, 'status');
+			if($temp != $attStatus) {
+				mysql_query("DELETE FROM attend WHERE attend.id=$eventID AND attend.attendee=$userID");
+				mysql_query("INSERT INTO attend (id, attendee, status) VALUES ('$eventID','$userID','$attStatus')");
+			}
 	}
-	
+	}
 	else
 	{
 		$status = "FAILURE";
-		echo $success;
+		echo $status;
 	}
 	
 
